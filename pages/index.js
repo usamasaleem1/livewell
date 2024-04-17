98e8ebaa369a695a7342b6f81352a93a68a822d1
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2024-04-17 13:03:55 -0400

    Added styling to home page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import Link from "next/link";
+import styles from "../styles/Welcome.module.css"; // Assuming you create a CSS module
 
 export default function Welcome() {
   return (
-    <div>
-      <h1> Welcome to Livewell </h1> <p> Are you a Doctor or a Patient ? </p>{" "}
-      <Link href="/selection?type=Patient">
-        <button
-          style={{
-            backgroundColor: "#2F9132",
-          }}
-        >
-          {" "}
-          Doctor{" "}
-        </button>{" "}
-      </Link>{" "}
-      <Link href="/selection?type=Doctor">
-        <button> Patient </button>{" "}
-      </Link>{" "}
+    <div className={styles.container}>
+      <h1> Welcome to Livewell </h1>{" "}
+      <img
+        src="https://img.icons8.com/?size=512&id=Mb4z4jh0wGon&format=png"
+        alt="Doctor"
+      />{" "}
+      <p style={{ textAlign: "center" }}>
+        Connect seamlessly with healthcare professionals or manage your patients
+        more efficiently. Are you a doctor or a patient? Choose your path below!
+      </p>{" "}
+      <div className={styles.buttonContainer}>
+        <Link href="/selection?type=Patient">
+          <button className={styles.doctorButton}>👨‍⚕️ I'm a Doctor</button>{" "}
+        </Link>{" "}
+        <Link href="/selection?type=Doctor">
+          <button className={styles.patientButton}>👱 I'm a Patient</button>{" "}
+        </Link>{" "}
+      </div>{" "}
     </div>
   );
 }
