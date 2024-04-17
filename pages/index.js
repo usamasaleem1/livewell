1d2113d7f2fe2d9f5e0c6b496710da23b2ce3214
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2024-04-17 13:08:01 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,22 @@ import styles from "../styles/Welcome.module.css"; // Assuming you create a CSS 
 export default function Welcome() {
   return (
     <div className={styles.container}>
-      <h1> Welcome to Livewell </h1>{" "}
+      <h1
+        style={{
+          marginBottom: "30px",
+        }}
+      >
+        Welcome to Livewell
+      </h1>{" "}
       <img
-        src="https://img.icons8.com/?size=512&id=Mb4z4jh0wGon&format=png"
+        src="https://img.icons8.com/?size=512&id=M2I2DdwQf1gB&format=png"
         alt="Doctor"
+        style={{
+          marginBottom: "20px",
+          boxShadow: "0 2px 15px 5px #00000000",
+        }}
       />{" "}
-      <p style={{ textAlign: "center" }}>
+      <p style={{ textAlign: "center", fontWeight: "bold", lineHeight: "1.5" }}>
         Connect seamlessly with healthcare professionals or manage your patients
         more efficiently. Are you a doctor or a patient? Choose your path below!
       </p>{" "}
